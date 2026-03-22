54ee4b0158cc450e9b87f1f55259bf41736c13cc
Hypn0tised-ysy <1481435885@qq.com>
2026-03-22 15:18:03 +0800

    feat: 导入prefabs/arts美术资源，地形批量生成脚本

---
+++ Assets/Scripts/Terrain/TerrainGridGenerator.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class TerrainGridGenerator : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [SerializeField] private GameObject terrain;
+    [SerializeField] private List<GameObject> builtTerrain = new List<GameObject>();
+    [SerializeField] private string terrainName = "Terrain";
+    [SerializeField] private float terrainWidth = 1f;
+    [SerializeField] private Vector2Int size;
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    { 
+        Undo.RegisterCompleteObjectUndo(this, "Generate Terrain");
+        Clear();
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                GameObject go = PrefabUtility.InstantiatePrefab(terrain, this.transform) as GameObject;
+                Undo.RegisterCreatedObjectUndo(go, "Generate Terrain");
+                go.name = terrainName + " " + x + ", " + y;
+                var transform1 = transform;
+                go.transform.position = transform1.position + (terrainWidth * x * transform1.right) + (terrainWidth * y * transform1.forward);
+                builtTerrain.Add(go);
+            }
+        }
+    }
+
+    [ContextMenu("Clear")]
+    public void Clear()
+    {
+        Undo.RegisterCompleteObjectUndo(this, "Clear Terrain");
+        for (int i = 0; i < builtTerrain.Count; i++)
+        {
+            GameObject go = builtTerrain[i];
+            if (go != null)
+            {
+                Undo.DestroyObjectImmediate(go);
+            }
+        }
+
+        builtTerrain.Clear();
+        this.transform.DestroyImmediateChildren();
+    }
+
+#endif
+}
