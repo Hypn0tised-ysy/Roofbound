4b88a3e2a13fbd7f8b1ea39fc9076e9a250027ac
zwh123z <2969089969@qq.com>
2026-04-26 19:22:37 +0800

    feat:新增关卡选择和胜利结算面板

---
+++ Assets/Scripts/UI_Manager/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 点击 "Start Game" 按钮绑定此方法
+    public void OnClickStart()
+    {
+        // 自己隐藏，请求总控打开选关界面
+        gameObject.SetActive(false);
+        UI_Controller.Instance.LevelSelect.ShowPanel();
+    }
+
+    public void ShowPanel()
+    {
+        gameObject.SetActive(true);
+        // 主菜单需要鼠标操作
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
